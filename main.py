7fe9d6d5449e4ec23df95a4ff761c339cf71bb8e
oswi <oswaldo.adrian@live.com.mx>
2022-10-30 19:46:38 -0600

    added emotions feature

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 \
-    import Features, CategoriesOptions, ConceptsOptions
+    import Features, CategoriesOptions, ConceptsOptions, EmotionOptions
 
 load_dotenv()
 authenticator = IAMAuthenticator(os.environ["apikey"])
@@ -18,6 +18,6 @@ response = natural_language_understanding.analyze(
     text='IBM is an American multinational technology company '
     'headquartered in Armonk, New York, United States, '
     'with operations in over 170 countries.',
-    features=Features(categories=CategoriesOptions(limit=3), concepts=ConceptsOptions(limit=3))).get_result()
+    features=Features(categories=CategoriesOptions(limit=3), concepts=ConceptsOptions(limit=3), emotion=EmotionOptions())).get_result()
 
 print(json.dumps(response, indent=2))
