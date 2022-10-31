dea28ed31eb24235103c7acb3b28de695ddba4db
oswi <oswaldo.adrian@live.com.mx>
2022-10-30 19:51:35 -0600

    text input

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from ibm_watson.natural_language_understanding_v1 \
     import Features, CategoriesOptions, ConceptsOptions, EmotionOptions
 
 load_dotenv()
+text = input("Enter your text: ")
 authenticator = IAMAuthenticator(os.environ["apikey"])
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2022-04-07',
@@ -15,9 +16,7 @@ natural_language_understanding = NaturalLanguageUnderstandingV1(
 )
 natural_language_understanding.set_service_url(os.environ["url"])
 response = natural_language_understanding.analyze(
-    text='IBM is an American multinational technology company '
-    'headquartered in Armonk, New York, United States, '
-    'with operations in over 170 countries.',
+    text=text,
     features=Features(categories=CategoriesOptions(limit=3), concepts=ConceptsOptions(limit=3), emotion=EmotionOptions())).get_result()
 
 print(json.dumps(response, indent=2))
